73ffb9af4b738406ee0b59da645651c3668bfcb8
thzw1142 <thzw1142@163.com>
2019-10-04 23:49:59 +0800

    test multi consumer and producer

--- src/main/java/com/imooc/disruptor/chapter3/multi/Main.java
+++ src/main/java/com/imooc/disruptor/chapter3/multi/Main.java
@@ -3,11 +3,14 @@ package com.imooc.disruptor.chapter3.multi;
 import com.lmax.disruptor.*;
 import com.lmax.disruptor.dsl.ProducerType;
 
+import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         //1.创建RingBuffer
         RingBuffer<Order> ringBuffer = RingBuffer.create(
@@ -58,7 +61,40 @@ public class Main {
         ringBuffer.addGatingSequences(orderWorkerPool.getWorkerSequences());
 
         //6.启动workpool
-        orderWorkerPool.start(Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors()));
+        ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        orderWorkerPool.start(pool);
+
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        //7. 设置100个生产者
+        for (int i = 0; i < 100; i++) {
+            final Producer producer = new Producer(ringBuffer);
+            new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+                    try {
+                        latch.await();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    for (int j = 0; j < 100; j++) {
+                        producer.sendData(UUID.randomUUID().toString());
+                    }
+                }
+
+            }).start();
+
+        }
+
+        Thread.sleep(2000);
+        System.out.println("----------------线程创建完毕, 开始生产数据---------------");
+        latch.countDown();
+
+        Thread.sleep(5000);
+
+        System.out.println("消费者处理的任务总数： " + consumers[0].getCount());
+        pool.shutdown();
 
     }
 
